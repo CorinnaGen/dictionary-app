9a966a4a96813330687210139465424ee2cea867
CorinnaGen <77247943+CorinnaGen@users.noreply.github.com>
2021-05-01 21:38:51 +0200

    css

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Dictionary</h1>
       </header>
+      <h1>Dictionary</h1>
+      <input></input>
     </div>
   );
 }
