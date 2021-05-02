d2c7a1099d17d49e1c6d1b177e15858d68baafff
CorinnaGen <77247943+CorinnaGen@users.noreply.github.com>
2021-05-02 21:11:06 +0200

    Update Results.js

--- src/Results.js
+++ src/Results.js
@@ -4,7 +4,9 @@ import "./Results.css";
 export default function showResults(props){
     if(props.result){
     return (<div className="Results"><h2>{props.results.word}</h2>
-    
+    {props.results.meanings.map(function(meaning, index){
+        return meaning.definitions[0].definition[0];
+     })}
 </div>)
 } else{
     return (<div className="noResults">We don't know what that means either</div>);}}
